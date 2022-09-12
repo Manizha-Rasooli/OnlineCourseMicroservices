6c47e0a7cd79a97d6c990800b7bf4160390f226a
manizha <manizha.rasouly@gmail.com>
2022-09-12 23:56:01 +0300

    TokenDelegate Handler is created

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Web.Handler;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Implementation;
 using FreeCourse.Web.Services.Interfaces;
@@ -35,7 +36,7 @@ namespace FreeCourse.Web
             services.AddHttpClient<IUserService, UserService>(opt =>
             {
                 opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
-            });
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(
                 CookieAuthenticationDefaults.AuthenticationScheme, opts =>
