6b380d8fc1ead904ec68c0b1adc00d6639329057
manizha <manizha.rasouly@gmail.com>
2022-09-17 19:49:57 +0300

    Implemented Methods of BasketService

---
+++ Frontends/FreeCourse.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Web.Handler;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Implementation;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddHttpClientServices(this IServiceCollection services, IConfiguration Configuration) {
+            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
+
+            services.AddHttpClient<IIdentityService, IdentityService>();
+
+            var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+
+            #region Catalog Service
+            services.AddHttpClient<ICatalogService, CatalogService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
+            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+            #endregion
+
+            #region PhotoStock Service
+            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.PhotoStock.Path}");
+            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+            #endregion
+
+            #region Basket Service
+            services.AddHttpClient<IBasketService, BasketService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Basket.Path}");
+            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+            #endregion
+
+            services.AddHttpClient<IUserService, UserService>(opt =>
+            {
+                opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+        }
+    }
+}
